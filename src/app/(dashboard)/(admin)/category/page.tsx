c750addf0cd1022ba10fb2b27e08ac8244c3ac4a
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-09 11:42:36 +0800

    feat(category) : add category page and its error page

---
+++ src/app/(dashboard)/(admin)/category/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import CategoryList from "@/features/category/components/category-list";
+import { Plus } from "lucide-react";
+
+export default async function CategoryPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-semibold mb-4">Category Page</h1>
+      <CategoryList />
+      <Link href="/category/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
+        <Plus />
+      </Link>
+    </div>
+  );
+}
