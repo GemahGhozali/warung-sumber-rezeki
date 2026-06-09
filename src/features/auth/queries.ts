6a4da5805ca580a1dd9776d3bf267d2a197c3b9b
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-09 11:54:54 +0800

    fix(auth) : add try-catch block for queries

--- src/features/auth/queries.ts
+++ src/features/auth/queries.ts
@@ -1,5 +1,9 @@
 import prisma from "@/libs/prisma";
 
-export async function getUserByUsername(identifier: string) {
-  return await prisma.user.findFirst({ where: { username: identifier } });
+export async function getUserByUsername(username: string) {
+  try {
+    return await prisma.user.findFirst({ where: { username } });
+  } catch (error) {
+    throw new Error("Gagal mendapatkan data pengguna");
+  }
 }
