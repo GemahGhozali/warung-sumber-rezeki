d9c9581449a35952bf978c983596ff2710e7e275
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-09 10:22:15 +0800

    feat(category) : add category queries

---
+++ src/features/category/queries.ts
@@ -0,0 +1,31 @@
+import prisma from "@/libs/prisma";
+
+export async function getAllCategories() {
+  try {
+    const categories = await prisma.category.findMany({
+      select: {
+        id: true,
+        name: true,
+        information: true,
+        createdAt: true,
+        updatedAt: true,
+        _count: { select: { menus: true } },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return categories.map(({ _count, ...categoryData }) => ({ ...categoryData, totalMenu: _count.menus }));
+  } catch (error) {
+    console.log(error);
+    throw new Error("Gagal mendapatkan data kategori");
+  }
+}
+
+export async function getCategoryById(id: string) {
+  try {
+    return prisma.category.findUnique({ where: { id } });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Gagal mendapatkan data kategori");
+  }
+}
