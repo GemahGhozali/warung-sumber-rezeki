b0b9de992eb7f695e08420ce039e50d64eeba7dc
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-10 19:25:14 +0800

    feat(user) : add user page and its error page

---
+++ src/app/(dashboard)/(admin)/user/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import UserList from "@/features/user/components/user-list";
+import { Plus } from "lucide-react";
+
+export default async function UserPage() {
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl">User Page</h1>
+      <UserList />
+      <Link href="/user/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
+        <Plus />
+      </Link>
+    </div>
+  );
+}
