33d4b95bfb4f6bbbda91a3c262ace5a8c2072a24
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-11 08:56:04 +0800

    feat(menu) : add menu page and its error page

---
+++ src/app/(dashboard)/(admin)/menu/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import MenuList from "@/features/menu/components/menu-list";
+import { Plus } from "lucide-react";
+
+export default async function MenuPage() {
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl">Menu Page</h1>
+      <MenuList />
+      <Link href="/menu/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
+        <Plus />
+      </Link>
+    </div>
+  );
+}
