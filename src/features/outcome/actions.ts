964879fc87d82525301409664ba34d36dfa36a88
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 22:50:58 +0800

    feat(outcome) : add outcome actions

---
+++ src/features/outcome/actions.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import prisma from "@/libs/prisma";
+import { getSession } from "@/libs/session";
+import { getActiveShiftId } from "@/features/shift/queries";
+import { OutcomeSchema, OutcomeInput } from "./schemas";
+import { ServerActionResponse } from "@/types";
+import { formatZodError } from "@/libs/zod";
+import { sendErrorResponse, sendSuccessResponse } from "@/utils/response";
+
+export async function createOutcomeAction(prevState: ServerActionResponse<OutcomeInput> | null, data: OutcomeInput): Promise<ServerActionResponse<OutcomeInput>> {
+  const validation = OutcomeSchema.safeParse(data);
+  if (!validation.success) {
+    return sendErrorResponse({
+      message: "Gagal mencatat pengeluaran",
+      code: "VALIDATION_ERROR",
+      fields: formatZodError(validation.error),
+    });
+  }
+
+  const { category, total, information } = validation.data;
+
+  const user = await getSession();
+  if (!user) return redirect("/");
+
+  try {
+    const activeShiftId = await getActiveShiftId();
+    if (!activeShiftId) return sendErrorResponse({ message: "Gagal mencatat pengeluaran. Anda harus membuka shift toko terlebih dahulu", code: "NOT_FOUND" });
+
+    await prisma.outcome.create({ data: { category, total, information, userId: user.id, shiftId: activeShiftId } });
+
+    revalidatePath("/", "layout");
+
+    return sendSuccessResponse({ message: "Berhasil mencatat pengeluaran kas" });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse({ message: "Terjadi kesalahan pada server saat mencatat pengeluaran", code: "INTERNAL_SERVER_ERROR" });
+  }
+}
