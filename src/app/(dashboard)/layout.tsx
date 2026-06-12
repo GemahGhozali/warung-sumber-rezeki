3a512679ad1c3dc36fcee058744169763c62f043
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 17:09:55 +0800

    refactor(dashboard) : add shift gate for page conditional rendering in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,20 @@
 import { redirect } from "next/navigation";
 import { getSession } from "@/libs/session";
+import { getActiveShiftId } from "@/features/shift/queries";
 import BottomNavigationBar from "@/component/bottom-navigation-bar";
+import ShiftGate from "@/features/shift/components/shift-gate";
 
 export default async function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const session = await getSession();
   if (!session) redirect("/");
 
+  const isShiftActive = await getActiveShiftId();
+
   return (
     <div className="relative flex h-full flex-col">
-      <div className="flex-1 overflow-y-auto">{children}</div>
+      <div className="flex-1 overflow-y-auto">
+        <ShiftGate isShiftActive={Boolean(isShiftActive)}>{children}</ShiftGate>
+      </div>
       <BottomNavigationBar role={session.role} />
     </div>
   );
