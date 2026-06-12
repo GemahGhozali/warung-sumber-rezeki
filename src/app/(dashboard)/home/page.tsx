8cc0ec10b32eae7bbc2d08a1f40287a76e5e1a4b
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 17:13:54 +0800

    refactor(home) : add shift overview in home page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,3 +1,15 @@
-export default function HomePage() {
-  return <div>Home Page</div>;
+import { getActiveShift } from "@/features/shift/queries";
+import ActionButtons from "@/features/home/components/action-buttons";
+import ShiftOverview from "@/features/home/components/shift-overview";
+
+export default async function HomePage() {
+  const shift = await getActiveShift();
+  if (!shift) return null;
+
+  return (
+    <div className="p-4">
+      <ActionButtons shift={shift} />
+      <ShiftOverview shift={shift} />
+    </div>
+  );
 }
