c8a78732747c04fcfd25811893c89d3610917f1e
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-13 07:43:59 +0800

    feat(arus-kas) : add basic temporary page for arus kas

--- src/app/(dashboard)/arus-kas/page.tsx
+++ src/app/(dashboard)/arus-kas/page.tsx
@@ -1,3 +1,21 @@
-export default function ArusKasPage() {
-  return <div>Arus Kas Page</div>;
+import ActionButtons from "@/features/arus-kas/components/action-buttons";
+import CashflowHistory from "@/features/arus-kas/components/cashflow-history";
+import { getActiveShift } from "@/features/shift/queries";
+import { formatCurrency } from "@/utils/format-currency";
+
+export default async function ArusKasPage() {
+  const cashflow = await getActiveShift();
+
+  if (!cashflow) return null;
+
+  return (
+    <div className="p-4 space-y-4">
+      <p>Kas Awal : {formatCurrency(cashflow.initialCapital)}</p>
+      <p>Total Pemasukan : + {formatCurrency(cashflow.totalIncomes)}</p>
+      <p>Total Pengeluaran : - {formatCurrency(cashflow.totalOutcomes)}</p>
+      <p>Kas Yang Tercatat : {formatCurrency(cashflow.expectedCash)}</p>
+      <ActionButtons currentCash={cashflow.expectedCash} />
+      <CashflowHistory />
+    </div>
+  );
 }
