54185d564671ff1bd0c2c5ba72e15b6947a63d6f
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 15:06:08 +0800

    refactor(user) : move all user pages to dashboard folder

--- src/app/(dashboard)/(admin)/dashboard/user/page.tsx
+++ src/app/(dashboard)/(admin)/dashboard/user/page.tsx
@@ -7,7 +7,7 @@ export default async function UserPage() {
     <div className="p-4 space-y-4">
       <h1 className="text-2xl">User Page</h1>
       <UserList />
-      <Link href="/user/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
+      <Link href="/dashboard/user/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
         <Plus />
       </Link>
     </div>
