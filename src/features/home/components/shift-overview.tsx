6254c2aeb404e4789bbae3fdb24cbd6111aa611a
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 22:37:24 +0800

    fix(home) : delete totalChanges field from shift overview component

--- src/features/home/components/shift-overview.tsx
+++ src/features/home/components/shift-overview.tsx
@@ -39,24 +39,17 @@ export default function ShiftOverview({ shift }: ShiftOverviewProps) {
           <p className="text-sm font-semibold text-neutral-500">{formatCurrency(shift.initialCapital)}</p>
         </div>
         <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Pemasukan</p>
+          <p className="text-sm text-neutral-500 font-semibold">Total Pemasukan (Bersih)</p>
           <p className={`text-sm font-semibold flex items-center gap-1 ${shift.totalIncomes > 0 ? "text-green-500" : "text-neutral-500"}`}>
             {shift.totalIncomes > 0 && <Plus size={16} strokeWidth={2.25} />}
             <span>{formatCurrency(shift.totalIncomes)}</span>
           </p>
         </div>
         <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Pemasukan</p>
+          <p className="text-sm text-neutral-500 font-semibold">Total Pengeluaran</p>
           <p className={`text-sm font-semibold flex items-center gap-1 ${shift.totalOutcomes > 0 ? "text-red-500" : "text-neutral-500"}`}>
             {shift.totalOutcomes > 0 && <Minus size={16} strokeWidth={2.25} />}
             <span>{formatCurrency(shift.totalOutcomes)}</span>
-          </p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Kembalian</p>
-          <p className={`text-sm font-semibold flex items-center gap-1 ${shift.totalChanges > 0 ? "text-red-500" : "text-neutral-500"}`}>
-            {shift.totalChanges > 0 && <Minus size={16} strokeWidth={2.25} />}
-            <span>{formatCurrency(shift.totalChanges)}</span>
           </p>
         </div>
         <hr className="border-neutral-300" />
