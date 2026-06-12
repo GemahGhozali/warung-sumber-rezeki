bfcf1df3648927f354dd536e17f47a600c747ab6
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 22:48:14 +0800

    feat(income) : add useCreateIncomeForm hook

---
+++ src/features/income/hooks.ts
@@ -0,0 +1,33 @@
+// src/features/outcome/hooks.ts
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useActionState, startTransition } from "react";
+import { IncomeSchema, IncomeFormInput, IncomeInput } from "./schemas";
+import { createIncomeAction } from "./actions";
+
+export function useCreateIncomeForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IncomeFormInput>({
+    resolver: zodResolver(IncomeSchema),
+    defaultValues: {
+      category: "MODAL_TAMBAHAN",
+      total: 0,
+      information: "",
+    },
+  });
+
+  const [state, formAction, isPending] = useActionState(createIncomeAction, null);
+
+  const onSubmitForm = (data: IncomeFormInput) => {
+    startTransition(() => {
+      formAction(data as IncomeInput);
+      reset();
+    });
+  };
+
+  return { register, handleSubmit: handleSubmit(onSubmitForm), errors, state, isPending };
+}
