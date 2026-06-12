d28a383d19ff6cbcd36f733e4e01eb36af16699e
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-12 17:08:17 +0800

    feat(shift) : add close shift empty state component

---
+++ src/features/shift/components/shift-empty-state.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRef } from "react";
+import { ModalHandle } from "@/component/modal";
+import OpenShiftModal from "./open-shift-modal";
+
+export default function ShiftEmptyState() {
+  const openShiftModalRef = useRef<ModalHandle>(null);
+  const handleOpenModal = () => openShiftModalRef.current?.openModal();
+  const handleCloseModal = () => openShiftModalRef.current?.closeModal();
+
+  return (
+    <div className="h-full flex flex-col justify-center items-center text-center">
+      <h5 className="text-lg font-semibold">Kamu belum membuka toko</h5>
+      <p className="text-neutral-500 text-sm mb-2">Silahkan buka toko untuk memulai operasional</p>
+      <button type="button" className="bg-teal-600 text-white rounded-lg px-4 py-2 font-medium cursor-pointer" onClick={handleOpenModal}>
+        Buka Toko Sekarang
+      </button>
+      <OpenShiftModal ref={openShiftModalRef} onClose={handleCloseModal} />
+    </div>
+  );
+}
