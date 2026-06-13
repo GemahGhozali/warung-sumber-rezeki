9ac8d618d7c9f0268a81da66670bb2aec3fde92d
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-13 16:04:35 +0800

    feat(menu) : add get menus for cashier query

--- src/features/menu/queries.ts
+++ src/features/menu/queries.ts
@@ -22,3 +22,17 @@ export async function getMenuById(id: string) {
     throw new Error("Gagal mendapatkan data menu");
   }
 }
+
+export async function getMenusForCashier() {
+  try {
+    const menus = await prisma.menu.findMany({
+      select: { id: true, name: true, price: true, hpp: true, image: true, category: { select: { name: true } } },
+      orderBy: { name: "asc" },
+    });
+
+    return menus.map((menu) => ({ ...menu, category: menu.category?.name }));
+  } catch (error) {
+    console.error(error);
+    throw new Error("Gagal mendapatkan data menu");
+  }
+}
