b2f5c306964bd6543067408b3191564fda93f7e6
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-14 19:26:00 +0800

    feat(kasir) : add category tab filters component

---
+++ src/features/kasir/components/category-tab-filters.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Category } from "@/generated/prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface CategoryTabFiltersProp {
+  categories: Pick<Category, "id" | "name">[];
+}
+
+export default function CategoryTabFilters({ categories }: CategoryTabFiltersProp) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentCategory = searchParams.get("category");
+
+  const handleCategoryClick = (categoryName: string | null) => {
+    const params = new URLSearchParams(searchParams.toString());
+    !categoryName ? params.delete("category") : params.set("category", categoryName);
+    router.push(`?${params.toString()}`);
+  };
+
+  const baseStyle = "py-4 cursor-pointer";
+  const activeStyle = "font-semibold text-teal-500 shadow-[inset_0_-3px_0_0_#14b8a6]";
+
+  return (
+    <div className="overflow-x-auto whitespace-nowrap px-4 scrollbar-hidden">
+      <div className="flex gap-8 w-max rounded-lg">
+        <button type="button" className={`${baseStyle} ${!currentCategory ? activeStyle : ""}`} onClick={() => handleCategoryClick(null)}>
+          Semua
+        </button>
+        {categories.map((category) => {
+          const isActive = currentCategory === category.name;
+          return (
+            <button key={category.id} type="button" className={`${baseStyle} ${isActive ? activeStyle : ""}`} onClick={() => handleCategoryClick(category.name)}>
+              {category.name}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
