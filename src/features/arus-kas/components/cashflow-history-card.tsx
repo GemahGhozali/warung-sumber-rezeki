f067445c42098767188870cd26bc22d9fbecc9a5
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-14 13:11:14 +0800

    fix(arus-kas) : fix time attribute in cashflow history card

--- src/features/arus-kas/components/cashflow-history-card.tsx
+++ src/features/arus-kas/components/cashflow-history-card.tsx
@@ -12,7 +12,7 @@ export default function CahsflowHistoryCard({ history }: CahsflowHistoryCardProp
     <li className="flex justify-between items-center p-3 border border-neutral-300 rounded-lg">
       <div>
         <h5 className="font-semibold text-sm mb-1">{history.label}</h5>
-        <p className="text-neutral-500 text-xs">{moment(history.amount).format("HH:mm A")}</p>
+        <p className="text-neutral-500 text-xs">{moment(history.createdAt).format("DD/MM/YYYY, HH:mm A")}</p>
       </div>
       <div className={`flex items-center gap-1 text-sm font-semibold ${history.type === "INCOME" ? "text-green-500" : "text-red-500"}`}>
         {history.type === "INCOME" ? <Plus size={16} strokeWidth={2.5} /> : <Minus size={16} strokeWidth={2.5} />}
