58588c2b9725140c6aa509359a880085945b0284
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-14 19:27:55 +0800

    feat(kasir) : add menu card component

---
+++ src/features/kasir/components/menu-card.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { CashierMenu } from "@/features/menu/types";
+import { formatCurrency } from "@/utils/format-currency";
+import MenuCardButtons from "./menu-card-button";
+
+interface MenuCardProps {
+  menu: CashierMenu;
+}
+
+export default function MenuCard({ menu }: MenuCardProps) {
+  return (
+    <li className="p-4 border-t border-neutral-300 flex items-center gap-4">
+      <img src={menu.image || "/images/menu-placeholder.png"} alt={menu.name} className="size-[60px] rounded-lg border border-neutral-300" />
+      <div className="space-y-1 mr-auto">
+        <h5 className="font-semibold">{menu.name}</h5>
+        <p className="text-neutral-500 font-semibold text-sm">{formatCurrency(menu.price)}</p>
+      </div>
+      <MenuCardButtons menu={menu} />
+    </li>
+  );
+}
