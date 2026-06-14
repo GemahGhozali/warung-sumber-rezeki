da01e8db3dfbbadfb1b5a091162ee39bf74457bc
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-14 19:34:13 +0800

    refactor(transaction) : update transaksi page to show trasaction list

--- src/app/(dashboard)/transaksi/page.tsx
+++ src/app/(dashboard)/transaksi/page.tsx
@@ -1,3 +1,11 @@
+import TransactionHistory from "@/features/transaction/components/transaction-history";
+
 export default function TransaksiPage() {
-  return <div>Transaksi Page</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-semibold">Riwayat Transaksi</h1>
+      <p className="text-sm text-neutral-500 mb-3">Lihat semua transaksi selama sesi operasional</p>
+      <TransactionHistory />
+    </div>
+  );
 }
