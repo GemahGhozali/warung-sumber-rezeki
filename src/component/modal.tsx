0c288d498fd5841ab5b7ef94cff7bd7022ffa7bb
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-16 06:12:58 +0800

    refactor(component) : add open (slide up) and close (slide down) animation for modal component

--- src/component/modal.tsx
+++ src/component/modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useImperativeHandle, useRef } from "react";
+import { useImperativeHandle, useRef, useState } from "react";
 import type { MouseEvent, ReactNode } from "react";
 
 export interface ModalHandle {
@@ -16,13 +16,23 @@ interface ModalProps {
 
 export function Modal({ ref, className = "", children }: ModalProps) {
   const modalRef = useRef<HTMLDialogElement>(null);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const executeClose = () => {
+    setIsClosing(true);
+
+    setTimeout(() => {
+      if (modalRef.current) {
+        modalRef.current.close();
+      }
+      setIsClosing(false);
+    }, 300);
+  };
 
   useImperativeHandle(ref, () => {
     return {
       closeModal: () => {
-        if (modalRef.current) {
-          modalRef.current.close();
-        }
+        executeClose();
       },
       openModal: () => {
         if (modalRef.current) {
@@ -34,11 +44,18 @@ export function Modal({ ref, className = "", children }: ModalProps) {
 
   const handleCloseModal = (e: MouseEvent<HTMLDialogElement>) => {
     if (!modalRef.current) return;
-    if (e.target === modalRef.current) modalRef.current.close();
+    if (e.target === modalRef.current) executeClose();
   };
 
   return (
-    <dialog ref={modalRef} className="backdrop:bg-black/30 fixed bottom-0 left-1/2 -translate-x-1/2 z-50 bg-transparent w-full max-w-[430px] h-fit my-0 mt-auto" onClick={handleCloseModal}>
+    <dialog
+      ref={modalRef}
+      className={`
+      fixed bottom-0 left-1/2 -translate-x-1/2 z-50 bg-transparent w-full max-w-[430px] h-fit my-0 mt-auto
+      backdrop:bg-black/30 backdrop:transition-opacity backdrop:duration-300 duration-300 ease-out starting:open:translate-y-full starting:open:opacity-0 ${isClosing ? "open:translate-y-full" : "open:translate-y-0"}
+    `}
+      onClick={handleCloseModal}
+    >
       <div className={`mx-auto ${className}`}>{children}</div>
     </dialog>
   );
