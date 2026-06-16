d980d909150e05fc6798fd84acab9b7f327cdea9
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-16 12:46:22 +0800

    refactor(kasir) : change radius value of menu card component

--- src/features/kasir/components/menu-card.tsx
+++ src/features/kasir/components/menu-card.tsx
@@ -10,8 +10,8 @@ interface MenuCardProps {
 
 export default function MenuCard({ menu }: MenuCardProps) {
   return (
-    <li className="p-4 border-t border-neutral-300 flex items-center gap-4">
-      <img src={menu.image || "/images/menu-placeholder.png"} alt={menu.name} className="size-[60px] rounded-lg border border-neutral-300" />
+    <li className="p-4 first:border-t-0 border-t border-neutral-300 flex items-center gap-4">
+      <img src={menu.image || "/images/menu-placeholder.png"} alt={menu.name} className="size-[60px] rounded-full border border-neutral-300" />
       <div className="space-y-1 mr-auto">
         <h5 className="font-semibold">{menu.name}</h5>
         <p className="text-neutral-500 font-semibold text-sm">{formatCurrency(menu.price)}</p>
