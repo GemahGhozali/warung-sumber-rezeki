22ab7819117f781dd064bda8d314a7005468d211
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-16 12:45:39 +0800

    refactor(kasir) : change custom menu modal button style

--- src/features/kasir/components/custom-menu-modal-button.tsx
+++ src/features/kasir/components/custom-menu-modal-button.tsx
@@ -1,19 +1,23 @@
 "use client";
 
-import { ModalHandle } from "@/component/modal";
-import { Utensils } from "lucide-react";
 import { useRef } from "react";
+import { ModalHandle } from "@/component/modal";
+import { Plus } from "lucide-react";
 import CustomMenuModal from "./custom-menu-modal";
 
-export default function CustomMenuModalButton() {
+interface CustomMenuModalButtonProps {
+  className?: string;
+}
+
+export default function CustomMenuModalButton({ className = "bg-teal-600 text-white" }: CustomMenuModalButtonProps) {
   const customMenuModalRef = useRef<ModalHandle>(null);
   const handleOpenCustomMenuModal = () => customMenuModalRef.current?.openModal();
   const handleCloseCustomMenuModal = () => customMenuModalRef.current?.closeModal();
 
   return (
     <>
-      <button className="grid place-content-center rounded-full size-10 border border-neutral-300 cursor-pointer" onClick={handleOpenCustomMenuModal}>
-        <Utensils size={20} />
+      <button className={`flex items-center gap-1.5 p-1.5 pl-3 pr-1.5 text-sm rounded-lg font-medium cursor-pointer ${className}`} onClick={handleOpenCustomMenuModal}>
+        Custom <Plus size={16} />
       </button>
       <CustomMenuModal ref={customMenuModalRef} onClose={handleCloseCustomMenuModal} />
     </>
