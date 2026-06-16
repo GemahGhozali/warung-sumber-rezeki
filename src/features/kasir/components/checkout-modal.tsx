a730c06d7d13dc661bd32647d11eaed70d0fa052
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-16 12:42:11 +0800

    refactor(kasir) : add header for custom menu modal component

--- src/features/kasir/components/checkout-modal.tsx
+++ src/features/kasir/components/checkout-modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { RefObject } from "react";
+import { Notes } from "@solar-icons/react-perf/BoldDuotone";
 import { Modal, ModalHandle } from "@/component/modal";
 import { PaymentMethodSelections } from "./payment-method-selections";
 import { useCheckoutForm } from "@/features/transaction/hooks";
@@ -17,7 +18,9 @@ export default function CheckoutModal({ ref, onClose }: CheckoutModalProps) {
   return (
     <Modal ref={ref} className="bg-white p-4 pt-6 rounded-t-2xl">
       <div className="flex flex-col items-center mb-8">
-        <div className="size-20 bg-teal-100/60 grid place-content-center rounded-full text-4xl leading-none mb-4">💵</div>
+        <div className="size-20 bg-teal-100/60 grid place-content-center rounded-full text-4xl leading-none mb-4">
+          <Notes size={40} color="#009689" />
+        </div>
         <p className="text-sm text-neutral-500 font-semibold">Total Keseluruhan</p>
         <h4 className="text-2xl font-semibold text-teal-600">{formatCurrency(totalPrice)}</h4>
       </div>
@@ -27,7 +30,7 @@ export default function CheckoutModal({ ref, onClose }: CheckoutModalProps) {
         {selectedPaymentMethod === "TUNAI" && (
           <>
             <div className="space-y-2">
-              <label className="block font-semibold text-sm">
+              <label className="block font-semibold">
                 Nominal Pembayaran <span className="text-red-600">*</span>
               </label>
               <input type="number" {...register("totalPayment", { valueAsNumber: true })} placeholder="Masukkan total pembayaran disini..." disabled={isPending} className="w-full py-2 px-3 rounded-lg border border-neutral-300" />
@@ -35,7 +38,7 @@ export default function CheckoutModal({ ref, onClose }: CheckoutModalProps) {
             </div>
 
             <div className="space-y-2">
-              <label className="block font-semibold text-sm">Total Kembalian</label>
+              <label className="block font-semibold">Total Kembalian (Otomatis)</label>
               <div className={`w-full py-2 px-3 rounded-lg border border-neutral-300 font-medium ${totalChange > 0 ? "text-red-600" : "text-neutral-500"}`}>{formatCurrency(totalChange)}</div>
             </div>
           </>
