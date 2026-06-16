0acd73846c3ffd259b84db5e998a3dd8c991cde4
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-16 08:29:13 +0800

    refactor(home) : add cashflow overview component to home page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -2,6 +2,7 @@ import { getActiveShift } from "@/features/shift/queries";
 import ActionButtons from "@/features/home/components/action-buttons";
 import ShiftOverview from "@/features/home/components/shift-overview";
 import LastTransactionHistory from "@/features/home/components/last-transaction-history";
+import CashflowOverview from "@/features/home/components/cashflow-overview";
 
 export default async function HomePage() {
   const shift = await getActiveShift();
@@ -11,6 +12,7 @@ export default async function HomePage() {
     <div className="p-4">
       <ActionButtons shift={shift} />
       <ShiftOverview shift={shift} />
+      <CashflowOverview shift={shift} />
       <LastTransactionHistory />
     </div>
   );
