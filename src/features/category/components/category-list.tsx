4f5dbb8731504263afb351287962fa49453a0235
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 12:24:17 +0800

    refactor(category) : add delete confirmation modal logic, add empty state, and change category list to client component

--- src/features/category/components/category-list.tsx
+++ src/features/category/components/category-list.tsx
@@ -1,16 +1,74 @@
+"use client";
+
+import { deleteCategoryAction } from "../actions";
+import { startTransition, useRef, useState } from "react";
+import { Tag } from "@solar-icons/react-perf/BoldDuotone";
+import { CategoryCatalog, CategoryCatalogItem } from "../types";
+import { ModalHandle } from "@/component/modal";
 import CategoryCard from "./category-card";
-import { getAllCategories } from "../queries";
+import DeleteConfirmationModal from "@/component/delete-confirmation-modal";
 
-export default async function CategoryList() {
-  const categories = await getAllCategories();
+interface CategoryListProps {
+  categories: CategoryCatalog;
+}
 
-  if (categories.length === 0) return <p className="text-neutral-500">Belum ada kategori apapun saat ini</p>;
+type CategoryState = Pick<CategoryCatalogItem, "id" | "name">;
+
+export default function CategoryList({ categories }: CategoryListProps) {
+  if (categories.length === 0) {
+    return (
+      <div className="p-4 grow overflow-y-auto scrollbar-hidden flex flex-col justify-center items-center">
+        <div className="bg-teal-100/60 size-[60px] rounded-full grid place-content-center mb-3">
+          <Tag size={32} color="#009689" />
+        </div>
+        <h6 className="font-semibold mb-0.5">Belum ada kategori menu apapun</h6>
+        <p className="text-neutral-500 text-sm">Silahkan tambahkan kategori menu</p>
+      </div>
+    );
+  }
+
+  const deleteModalRef = useRef<ModalHandle>(null);
+  const [selectedCategory, setSelectedCategory] = useState<CategoryState | null>(null);
+
+  const handleCloseDeleteModal = () => {
+    setSelectedCategory(null);
+    deleteModalRef.current?.closeModal();
+  };
+
+  const handleOpenDeleteModal = (category: CategoryState) => {
+    setSelectedCategory(category);
+    deleteModalRef.current?.openModal();
+  };
+
+  const handleConfirmDelete = () => {
+    if (!selectedCategory) return;
+
+    startTransition(async () => {
+      await deleteCategoryAction(selectedCategory.id);
+      deleteModalRef.current?.closeModal();
+      setSelectedCategory(null);
+    });
+  };
 
   return (
-    <ul className="space-y-4">
-      {categories.map((category) => (
-        <CategoryCard key={category.id} category={category} />
-      ))}
-    </ul>
+    <>
+      <ul className="pb-4 space-y-4">
+        {categories.map((category) => (
+          <CategoryCard
+            key={category.id}
+            category={category}
+            onDelete={() =>
+              handleOpenDeleteModal({
+                id: category.id,
+                name: category.name,
+              })
+            }
+          />
+        ))}
+      </ul>
+      <DeleteConfirmationModal ref={deleteModalRef} onClose={handleCloseDeleteModal} onDelete={handleConfirmDelete}>
+        Kategori dengan nama <span className="font-semibold">{selectedCategory?.name}</span> akan dihapus secara permananen!
+      </DeleteConfirmationModal>
+    </>
   );
 }
