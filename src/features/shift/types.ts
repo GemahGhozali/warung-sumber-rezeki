7add4646be545beb2842307dafef0f084c5910a5
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 15:35:22 +0800

    feat(shift) : add shift details types

--- src/features/shift/types.ts
+++ src/features/shift/types.ts
@@ -1,5 +1,6 @@
-import { getActiveShift, getActiveShiftCashflowHistory, getAllShifts } from "./queries";
+import { getActiveShift, getActiveShiftCashflowHistory, getAllShifts, getShiftById } from "./queries";
 
 export type ActiveShift = NonNullable<Awaited<ReturnType<typeof getActiveShift>>>;
 export type ShiftHistory = NonNullable<Awaited<ReturnType<typeof getAllShifts>>>[number];
 export type CashflowHistory = NonNullable<Awaited<ReturnType<typeof getActiveShiftCashflowHistory>>>[number];
+export type ShiftDetails = NonNullable<Awaited<ReturnType<typeof getShiftById>>>;
