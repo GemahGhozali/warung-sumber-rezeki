47e2e0ad329d3a672d6a6c1f81114d51b0f4548f
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 13:10:08 +0800

    refactor(user) : fetch user data in user page and integrate it with user header

--- src/app/(dashboard)/(admin)/dashboard/user/page.tsx
+++ src/app/(dashboard)/(admin)/dashboard/user/page.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
 import UserList from "@/features/user/components/user-list";
-import { Plus } from "lucide-react";
+import UserHeader from "@/features/user/components/user-header";
+import { getAllUsers } from "@/features/user/queries";
 
 export default async function UserPage() {
+  const users = await getAllUsers();
+
   return (
-    <div className="p-4 space-y-4">
-      <h1 className="text-2xl">User Page</h1>
-      <UserList />
-      <Link href="/dashboard/user/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
-        <Plus />
-      </Link>
+    <div className="p-4 h-full flex flex-col">
+      <UserHeader />
+      <UserList users={users} />
     </div>
   );
 }
