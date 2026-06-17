16ef4fc1a3117645e07823cab25961dfd59680cd
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 20:32:45 +0800

    refactor(shift) : add store illustration in shift empty state component

--- src/features/shift/components/shift-empty-state.tsx
+++ src/features/shift/components/shift-empty-state.tsx
@@ -11,6 +11,7 @@ export default function ShiftEmptyState() {
 
   return (
     <div className="h-full flex flex-col justify-center items-center text-center">
+      <img src="/images/store-illustration.png" className="size-[100px] mb-3" />
       <h5 className="text-lg font-semibold">Kamu belum membuka toko</h5>
       <p className="text-neutral-500 text-sm mb-2">Silahkan buka toko untuk memulai operasional</p>
       <button type="button" className="bg-teal-600 text-white rounded-lg px-4 py-2 font-medium cursor-pointer" onClick={handleOpenModal}>
