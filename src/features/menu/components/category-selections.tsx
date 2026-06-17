28cb0b385fbd8d86bd9f22d8008adf06453b242b
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 13:34:20 +0800

    refactor(menu) : update category selections menu style and it props type

--- src/features/menu/components/category-selections.tsx
+++ src/features/menu/components/category-selections.tsx
@@ -1,9 +1,10 @@
 import { MenuFormInput } from "../schemas";
 import { UseFormRegister, FieldErrors } from "react-hook-form";
-import { Category } from "@/generated/prisma/client";
+import { ChevronDown } from "lucide-react";
+import { CategoryCatalog } from "@/features/category/types";
 
 interface RoleSelectionsProps {
-  categories: Category[];
+  categories: CategoryCatalog;
   register: UseFormRegister<MenuFormInput>;
   errors: FieldErrors<MenuFormInput>;
   disabled?: boolean;
@@ -12,15 +13,18 @@ interface RoleSelectionsProps {
 export default function CategorySelections({ categories, register, errors, disabled }: RoleSelectionsProps) {
   return (
     <div className="space-y-1">
-      <label className="block font-semibold">Kategori Menu</label>
-      <select {...register("categoryId")} disabled={disabled} className="w-full py-2 px-3 rounded-lg border border-neutral-300 bg-white">
-        <option value="">--- Pilih Kategori (Opsional) ---</option>
-        {categories.map((category) => (
-          <option key={category.id} value={category.id}>
-            {category.name}
-          </option>
-        ))}
-      </select>
+      <label className="block font-semibold">Kategori Menu (Opsional)</label>
+      <div className="w-full relative">
+        <select {...register("categoryId")} disabled={disabled} className="peer w-full py-2 px-3 rounded-lg border border-neutral-300 bg-white appearance-none relative">
+          <option value="">Pilih Kategori</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+        <ChevronDown className="pointer-events-none absolute top-1/2 -translate-y-1/2 right-3 peer-focus:rotate-180 transition-transform duration-300" />
+      </div>
       {errors.categoryId && <small className="text-red-500 text-sm">{errors.categoryId.message}</small>}
     </div>
   );
