f6a98ef27d32ccafca7b5421d7d57523ba2be98d
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 16:05:31 +0800

    refactor(transaction) : add all seperate transaction component to transaction details page

--- src/app/(dashboard)/transaksi/[id]/page.tsx
+++ src/app/(dashboard)/transaksi/[id]/page.tsx
@@ -1,6 +1,8 @@
-import moment from "moment";
-import { formatCurrency } from "@/utils/format-currency";
 import { getTransactionById } from "@/features/transaction/queries";
+import TransactionDetailsHeader from "@/features/transaction/components/transaction-details-header";
+import TransactionPaymentMethods from "@/features/transaction/components/transaction-payment-methods";
+import TransactionInformation from "@/features/transaction/components/transaction-information";
+import TransactionItemList from "@/features/transaction/components/transaction-item-list";
 
 interface TransactionDetailsPageProps {
   params: Promise<{ id: string }>;
@@ -8,54 +10,18 @@ interface TransactionDetailsPageProps {
 
 export default async function TransactionDetailsPage({ params }: TransactionDetailsPageProps) {
   const { id } = await params;
-  const trasaction = await getTransactionById(id);
+  const transaction = await getTransactionById(id);
 
-  if (!trasaction) throw new Error("Data transaksi tidak ditemukan");
+  if (!transaction) throw new Error("Data transaksi tidak ditemukan");
 
   return (
-    <div className="p-4">
-      <p className="text-neutral-500 mb-2 font-semibold">Informasi Transaksi</p>
-      <div className="p-3 border border-neutral-300 rounded-lg space-y-3 mb-8">
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Karyawan</p>
-          <p className="text-sm font-semibold">{trasaction.employee}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Tanggal Transaksi</p>
-          <p className="text-sm font-semibold">{moment(trasaction.createdAt).format("DD/MM/YYYY, HH:mm A")}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Item</p>
-          <p className="text-sm font-semibold">{trasaction.totalItem}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Harga</p>
-          <p className="text-sm font-semibold">{formatCurrency(trasaction.totalPrice)}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Pembayaran</p>
-          <p className="text-sm font-semibold">{formatCurrency(trasaction.totalPayment)}</p>
-        </div>
-        <div className="flex justify-between">
-          <p className="text-sm text-neutral-500 font-semibold">Total Kembalian</p>
-          <p className="text-sm font-semibold">{formatCurrency(trasaction.totalChange)}</p>
-        </div>
+    <div className="bg-teal-600">
+      <TransactionDetailsHeader transaction={transaction} />
+      <div className="p-4 bg-neutral-50 rounded-t-xl">
+        <TransactionPaymentMethods transaction={transaction} />
+        <TransactionInformation transaction={transaction} />
+        <TransactionItemList transaction={transaction} />
       </div>
-
-      <p className="text-neutral-500 mb-2 font-semibold">Item Transaksi</p>
-      <ul className="space-y-4">
-        {trasaction.transactionDetails.map((item) => (
-          <li key={item.id} className="flex justify-between items-end p-3 border border-neutral-300 rounded-lg">
-            <div className="space-y-1">
-              <h6 className="font-semibold">
-                {item.menuName} ({item.quantity}x)
-              </h6>
-              <p className="text-sm text-neutral-500 font-semibold">{formatCurrency(item.price)}</p>
-            </div>
-            <p className="text-teal-600 font-semibold text-sm">{formatCurrency(item.subtotal)}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
