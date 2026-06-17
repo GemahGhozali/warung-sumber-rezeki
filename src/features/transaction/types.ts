5d2c2b6d2379352f9f838e5da181474d4ddfaaa3
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 16:02:23 +0800

    feat(transaction) : add transaction details item type

--- src/features/transaction/types.ts
+++ src/features/transaction/types.ts
@@ -2,3 +2,4 @@ import { getAllTransactionsInActiveShift, getTransactionById } from "./queries";
 
 export type TransactionHistory = NonNullable<Awaited<ReturnType<typeof getAllTransactionsInActiveShift>>>[number];
 export type TransactionDetails = NonNullable<Awaited<ReturnType<typeof getTransactionById>>>;
+export type TransactionDetailsItem = TransactionDetails["transactionDetails"][number];
