26da1cf955cf10b8c30a956f60e66b3650128df6
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 12:22:36 +0800

    refactor(category) : update style of category form by adding stars mark for required field, add rows attribute for textarea and add conditional rendering for button

--- src/features/category/components/category-form.tsx
+++ src/features/category/components/category-form.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { X } from "lucide-react";
 import { CategoryInput } from "../schemas";
 import { useCreateCategoryForm, useEditCategoryForm } from "../hooks";
 
@@ -10,32 +9,32 @@ interface EditCategoryFormProps {
 }
 
 export default function CategoryForm({ category }: EditCategoryFormProps) {
-  const { register, handleSubmit, errors, state, isPending } = category ? useEditCategoryForm({ categoryData: category }) : useCreateCategoryForm();
+  const { register, handleSubmit, errors, isPending } = category ? useEditCategoryForm({ categoryData: category }) : useCreateCategoryForm();
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4 p-4">
+    <form onSubmit={handleSubmit} className="space-y-6 p-4 h-full flex flex-col">
       <div className="space-y-1">
-        <label className="block font-semibold">Nama Kategori</label>
+        <label className="block font-semibold">
+          Nama Kategori <span className="text-red-600">*</span>
+        </label>
         <input type="text" {...register("name")} placeholder="Masukkan nama kategori disini..." disabled={isPending} className="w-full py-2 px-3 rounded-lg border border-neutral-300" />
         {errors.name && <small className="text-red-500 text-sm">{errors.name.message}</small>}
       </div>
       <div className="space-y-1">
-        <label className="block font-semibold">Keterangan</label>
-        <textarea {...register("information")} placeholder="Masukkan keterangan disini..." disabled={isPending} className="w-full py-2 px-3 rounded-lg border border-neutral-300"></textarea>
+        <label className="block font-semibold">
+          Keterangan <span className="text-red-600">*</span>
+        </label>
+        <textarea {...register("information")} placeholder="Masukkan keterangan disini..." rows={3} disabled={isPending} className="w-full py-2 px-3 rounded-lg border border-neutral-300"></textarea>
         {errors.information && <small className="text-red-500 text-sm">{errors.information.message}</small>}
       </div>
-      <button type="submit" disabled={isPending} className="w-full bg-teal-600 text-white rounded-lg px-4 py-2 font-medium cursor-pointer">
-        Edit Kategori
-      </button>
-      <Link href="/dashboard/menu/category" className="flex justify-center w-full bg-white text-neutral-500 border border-neutral-300 rounded-lg px-4 py-2 font-medium">
-        Batalkan
-      </Link>
-      {!state?.success && state?.message && (
-        <div className="p-3 bg-red-100 rounded-lg flex items-center gap-1.5">
-          <X size={24} className="text-red-500" />
-          <p className="text-red-500">{state.message}</p>
-        </div>
-      )}
+      <div className="space-y-4 mt-auto">
+        <button type="submit" disabled={isPending} className="w-full bg-teal-600 text-white rounded-lg px-4 py-2 font-medium cursor-pointer">
+          {category ? "Edit Kategori" : "Tambah Kategori"}
+        </button>
+        <Link href="/dashboard/menu/category" className="flex justify-center w-full bg-white text-neutral-500 border border-neutral-300 rounded-lg px-4 py-2 font-medium">
+          Batalkan
+        </Link>
+      </div>
     </form>
   );
 }
