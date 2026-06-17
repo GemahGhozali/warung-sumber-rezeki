f676cd323d10e03d8b95c911d053d56cbfabfdb3
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 13:36:12 +0800

    refactor(menu) : update menu card style, change its props type, and remove delete action logic in menu card component

--- src/features/menu/components/menu-card.tsx
+++ src/features/menu/components/menu-card.tsx
@@ -2,37 +2,27 @@
 
 import Link from "next/link";
 import { Edit, Trash2Icon } from "lucide-react";
-import { Menu } from "@/generated/prisma/client";
-import { startTransition } from "react";
-import { deleteMenuAction } from "../actions";
+import { formatCurrency } from "@/utils/format-currency";
+import { MenuCatalogItem } from "../types";
 
 interface MenuCardProps {
-  menu: Pick<Menu, "id" | "name" | "price" | "image">;
+  menu: MenuCatalogItem;
+  onDelete: () => void;
 }
 
-export default function MenuCard({ menu }: MenuCardProps) {
-  const handleDelete = () => {
-    const isConfirmed = confirm(`Apakah Anda yakin ingin menghapus menu "${menu.name}"?`);
-    if (!isConfirmed) return;
-
-    startTransition(async () => {
-      const response = await deleteMenuAction(menu.id);
-      if (response && !response.success) alert(response.message);
-    });
-  };
-
+export default function MenuCard({ menu, onDelete }: MenuCardProps) {
   return (
-    <li key={menu.id} className="flex items-center gap-3">
-      <img src={menu.image || "/images/menu-placeholder.png"} alt={menu.name} className="size-[45px] rounded-full object-cover" />
-      <div>
+    <li key={menu.id} className="flex items-center gap-3 p-3 bg-white border border-neutral-300 rounded-2xl">
+      <img src={menu.image || "/images/menu-placeholder.png"} alt={menu.name} className="size-[50px] rounded-full object-cover border border-neutral-300" />
+      <div className="space-y-0.5">
         <h6 className="font-semibold">{menu.name}</h6>
-        <p className="text-neutral-500 text-sm">{menu.price}</p>
+        <p className="text-neutral-500 text-sm">{formatCurrency(menu.price)}</p>
       </div>
       <div className="flex gap-2 ml-auto">
-        <Link href={`/dashboard/menu/${menu.id}/edit`} className="size-9 grid place-content-center bg-teal-50 text-teal-600 rounded-lg">
+        <Link href={`/dashboard/menu/${menu.id}/edit`} className="size-9 grid place-content-center bg-teal-100/50 text-teal-600 rounded-full">
           <Edit size={16} />
         </Link>
-        <button className="size-9 grid place-content-center bg-red-50 text-red-600 rounded-lg cursor-pointer" onClick={handleDelete}>
+        <button className="size-9 grid place-content-center bg-red-100/50 text-red-600 rounded-full cursor-pointer" onClick={onDelete}>
           <Trash2Icon size={16} />
         </button>
       </div>
