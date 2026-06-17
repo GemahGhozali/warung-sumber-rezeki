ef4f068326fa4ad673831a56480e9a33ba36a668
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 19:32:45 +0800

    refactor(arus-kas) : update style of cashflow history component and add its empty state

--- src/features/arus-kas/components/cashflow-history.tsx
+++ src/features/arus-kas/components/cashflow-history.tsx
@@ -1,18 +1,31 @@
 import CahsflowHistoryCard from "./cashflow-history-card";
 import { getActiveShiftCashflowHistory } from "@/features/shift/queries";
 
+import { WalletMoney } from "@solar-icons/react-perf/BoldDuotone";
 export default async function CashflowHistory() {
   const cashflowHistory = await getActiveShiftCashflowHistory();
 
+  if (cashflowHistory.length === 0) {
+    return (
+      <div className="p-6 flex flex-col h-full justify-center items-center rounded-2xl">
+        <div className="bg-teal-100/50 size-[60px] shrink-0 rounded-full grid place-content-center mb-3">
+          <WalletMoney size={32} color="#009689" />
+        </div>
+        <h6 className="font-semibold mb-0.5">Uang kas masih utuh</h6>
+        <p className="text-neutral-500 text-sm">Belum ada mutasi kas apapun saat ini</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1 className="text-xl font-semibold">Riwayat Arus Kas</h1>
-      <p className="text-neutral-500 text-sm mb-3">Lihat aliran arus kas selama sesi operasional</p>
+    <>
+      <h1 className="text-xl font-semibold mb-0.5">Riwayat Arus Kas</h1>
+      <p className="text-neutral-500 text-sm mb-4">Lihat aliran arus kas selama sesi operasional</p>
       <ul className="space-y-4">
         {cashflowHistory.map((history) => (
           <CahsflowHistoryCard key={history.id} history={history} />
         ))}
       </ul>
-    </div>
+    </>
   );
 }
