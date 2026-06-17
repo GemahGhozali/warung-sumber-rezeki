77de997e36f9986c064da667420e1ffe7db5e34d
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 12:28:27 +0800

    refactor(category) : add category header and add query function to get all categories in category page

--- src/app/(dashboard)/(admin)/dashboard/menu/category/page.tsx
+++ src/app/(dashboard)/(admin)/dashboard/menu/category/page.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
 import CategoryList from "@/features/category/components/category-list";
-import { Plus } from "lucide-react";
+import CategoryHeader from "@/features/category/components/category-header";
+import { getAllCategories } from "@/features/category/queries";
 
 export default async function CategoryPage() {
+  const categories = await getAllCategories();
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">Category Page</h1>
-      <CategoryList />
-      <Link href="/dashboard/menu/category/create" className="fixed bottom-4 right-4 bg-teal-600 text-white rounded-full font-medium size-10 grid place-content-center">
-        <Plus />
-      </Link>
+    <div className="p-4 h-full flex flex-col">
+      <CategoryHeader />
+      <CategoryList categories={categories} />
     </div>
   );
 }
