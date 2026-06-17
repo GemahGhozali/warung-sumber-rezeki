6f03f717424110c7b8dd60bdd0e1da5ff0fb8cf3
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 12:16:58 +0800

    refactor(category) : update category card style, remove delete action logic, and add onDelete props

--- src/features/category/components/category-card.tsx
+++ src/features/category/components/category-card.tsx
@@ -1,36 +1,30 @@
 "use client";
 
 import Link from "next/link";
+import { Tag } from "@solar-icons/react-perf/BoldDuotone";
+import { CategoryCatalogItem } from "../types";
 import { Edit, Trash2Icon } from "lucide-react";
-import { startTransition } from "react";
-import { deleteCategoryAction } from "../actions";
 
 interface CategoryCardProps {
-  category: { id: string; name: string; totalMenu: number };
+  category: CategoryCatalogItem;
+  onDelete: () => void;
 }
 
-export default function CategoryCard({ category }: CategoryCardProps) {
-  const handleDelete = () => {
-    const isConfirmed = confirm(`Apakah Anda yakin ingin menghapus kategori "${category.name}"?`);
-    if (!isConfirmed) return;
-
-    startTransition(async () => {
-      const response = await deleteCategoryAction(null, { id: category.id, name: category.name });
-      if (response && !response.success) alert(response.message);
-    });
-  };
-
+export default function CategoryCard({ category, onDelete }: CategoryCardProps) {
   return (
-    <li key={category.id} className="p-3 bg-white border border-neutral-300 rounded-lg flex items-center justify-between">
-      <div>
+    <li className="p-3 bg-white border border-neutral-300 rounded-lg flex items-center gap-3">
+      <div className="bg-teal-100/60 size-[50px] rounded-full grid place-content-center">
+        <Tag size={24} color="#009689" />
+      </div>
+      <div className="space-y-0.5">
         <h6 className="font-semibold">{category.name}</h6>
         <p className="text-neutral-500 text-sm">{category.totalMenu} Total Menu</p>
       </div>
-      <div className="flex gap-2">
-        <Link href={`/dashboard/menu/category/${category.id}/edit`} className="size-9 grid place-content-center bg-teal-50 text-teal-600 rounded-lg">
+      <div className="flex gap-2 ml-auto">
+        <Link href={`/dashboard/menu/category/${category.id}/edit`} className="size-9 grid place-content-center bg-teal-100/50 text-teal-600 rounded-full">
           <Edit size={16} />
         </Link>
-        <button className="size-9 grid place-content-center bg-red-50 text-red-600 rounded-lg cursor-pointer" onClick={handleDelete}>
+        <button className="size-9 grid place-content-center bg-red-100/50 text-red-600 rounded-full cursor-pointer" onClick={onDelete}>
           <Trash2Icon size={16} />
         </button>
       </div>
