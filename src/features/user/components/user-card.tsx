5ead56c58ffe8f12f72044a29ba4e8f3ff1278a8
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-17 12:56:16 +0800

    refactor(user) : update user card style, remove delete action logic and add onDelete props

--- src/features/user/components/user-card.tsx
+++ src/features/user/components/user-card.tsx
@@ -1,41 +1,31 @@
 "use client";
 
 import Link from "next/link";
-import { Crown, User as UserIcon, Edit, Trash2Icon } from "lucide-react";
-import { User } from "@/generated/prisma/client";
-import { startTransition } from "react";
-import { deleteUserAction } from "../actions";
+import { Edit, Trash2Icon } from "lucide-react";
+import { Crown, User as UserIcon } from "@solar-icons/react-perf/BoldDuotone";
+import { UserCatalogItem } from "../types";
 
 interface UserCardProps {
-  user: Pick<User, "id" | "name" | "role" | "image">;
+  user: UserCatalogItem;
+  onDelete: () => void;
 }
 
-export default function UserCard({ user }: UserCardProps) {
-  const handleDelete = () => {
-    const isConfirmed = confirm(`Apakah Anda yakin ingin menghapus pengguna "${user.name}"?`);
-    if (!isConfirmed) return;
-
-    startTransition(async () => {
-      const response = await deleteUserAction(user.id);
-      if (response && !response.success) alert(response.message);
-    });
-  };
-
+export default function UserCard({ user, onDelete }: UserCardProps) {
   return (
-    <li key={user.id} className="flex items-center gap-3">
-      <img src={user.image || "/images/avatar-placeholder.png"} alt={user.name} className="size-[45px] rounded-full object-cover" />
-      <div>
+    <li className="flex items-center gap-3 p-3 bg-white border border-neutral-300 rounded-2xl">
+      <img src={user.image || "/images/avatar-placeholder.png"} alt={user.name} className="size-[50px] rounded-full object-cover border border-neutral-300" />
+      <div className="space-y-0.5">
         <h6 className="font-semibold">{user.name}</h6>
         <div className="flex items-center gap-1.5 text-teal-600">
-          {user.role === "ADMIN" ? <Crown size={16} /> : <UserIcon size={16} strokeWidth={2.25} />}
+          {user.role === "ADMIN" ? <Crown size={20} /> : <UserIcon size={20} />}
           <p className="lowercase first-letter:uppercase text-sm text-neutral-500 font-medium">{user.role}</p>
         </div>
       </div>
       <div className="flex gap-2 ml-auto">
-        <Link href={`/dashboard/user/${user.id}/edit`} className="size-9 grid place-content-center bg-teal-50 text-teal-600 rounded-lg">
+        <Link href={`/dashboard/user/${user.id}/edit`} className="size-9 grid place-content-center bg-teal-100/50 text-teal-600 rounded-full">
           <Edit size={16} />
         </Link>
-        <button className="size-9 grid place-content-center bg-red-50 text-red-600 rounded-lg cursor-pointer" onClick={handleDelete}>
+        <button className="size-9 grid place-content-center bg-red-100/50 text-red-600 rounded-full cursor-pointer" onClick={onDelete}>
           <Trash2Icon size={16} />
         </button>
       </div>
