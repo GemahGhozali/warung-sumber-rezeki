4f03ad617bdb1836b78f0907204138369f44a41c
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-18 20:29:22 +0800

    refactor(dashboard) : integrate all visualization and statistic component, add search params for date filter logic and fetch profit loss report in dashboard page

--- src/app/(dashboard)/(admin)/dashboard/page.tsx
+++ src/app/(dashboard)/(admin)/dashboard/page.tsx
@@ -1,3 +1,35 @@
-export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+import DateFilter from "@/component/date-filter";
+import MetricCards from "@/features/dashboard/components/metric-cards";
+import OutcomeProportionChart from "@/features/dashboard/components/outcome-proportion-chart";
+import ProfitLossReports from "@/features/dashboard/components/profit-loss-reports";
+import { getProfitLossReport } from "@/features/dashboard/queries";
+
+interface DashboardPageProps {
+  searchParams: Promise<{
+    start?: string;
+    end?: string;
+  }>;
+}
+
+export default async function DashboardPage({ searchParams }: DashboardPageProps) {
+  const { start, end } = await searchParams;
+
+  const filterDate = {
+    startDate: start ? new Date(start) : undefined,
+    endDate: end ? new Date(end) : undefined,
+  };
+
+  const report = await getProfitLossReport(filterDate);
+  return (
+    <div className="p-4 space-y-4">
+      <div>
+        <h5 className="text-xl font-semibold">Laporan Laba Rugi</h5>
+        <p className="text-sm text-neutral-500">Lihat performa toko dan evaluasi hasil penjualan</p>
+      </div>
+      <DateFilter />
+      <MetricCards report={report} />
+      <ProfitLossReports report={report} />
+      <OutcomeProportionChart dataBeban={report.detail.bebanPengeluaran} totalBeban={report.ringkasan.totalBebanOperasional} />
+    </div>
+  );
 }
