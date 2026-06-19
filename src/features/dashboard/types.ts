f571714c0456040d0b33b4e8881b70ed6a6dfc9e
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-19 15:38:18 +0800

    feat(dashboard) : add shift audit cash difference type

--- src/features/dashboard/types.ts
+++ src/features/dashboard/types.ts
@@ -1,3 +1,4 @@
-import { getProfitLossReport } from "./queries";
+import { getShiftAuditCashDifference, getProfitLossReport } from "./queries";
 
 export type ProfitLossReport = NonNullable<Awaited<ReturnType<typeof getProfitLossReport>>>;
+export type ShiftAuditCashDifference = NonNullable<Awaited<ReturnType<typeof getShiftAuditCashDifference>>>[number];
