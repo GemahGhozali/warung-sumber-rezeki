45ce394c9c1905f74bf1215c3d2dcf0867326c41
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-20 09:11:38 +0800

    delete(transaction) : move all transaction queries that related with shift to shift features

--- src/features/transaction/queries.ts
+++ src/features/transaction/queries.ts
@@ -1,38 +1,4 @@
 import prisma from "@/libs/prisma";
-import { getActiveShiftId } from "../shift/queries";
-
-export async function getAllTransactionsInActiveShift() {
-  try {
-    const activeShiftId = await getActiveShiftId();
-    if (!activeShiftId) return null;
-
-    return prisma.transaction.findMany({
-      where: { shiftId: activeShiftId },
-      select: { id: true, paymentMethod: true, createdAt: true, totalPrice: true },
-      orderBy: { createdAt: "desc" },
-    });
-  } catch (error) {
-    console.error(error);
-    throw new Error("Gagal mendapatkan data riwayat transaksi");
-  }
-}
-
-export async function getLastTransactionsInActiveShift() {
-  try {
-    const activeShiftId = await getActiveShiftId();
-    if (!activeShiftId) return null;
-
-    return prisma.transaction.findMany({
-      where: { shiftId: activeShiftId },
-      select: { id: true, paymentMethod: true, createdAt: true, totalPrice: true },
-      orderBy: { createdAt: "desc" },
-      take: 5,
-    });
-  } catch (error) {
-    console.error(error);
-    throw new Error("Gagal mendapatkan data riwayat transaksi");
-  }
-}
 
 export async function getTransactionById(id: string) {
   try {
