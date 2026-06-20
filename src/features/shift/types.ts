c0543f8168ccb063f02906f58a94a9e03f08f710
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-20 09:26:27 +0800

    fix(shift) : adjust imported function to a new function name

--- src/features/shift/types.ts
+++ src/features/shift/types.ts
@@ -1,6 +1,6 @@
-import { getActiveShift, getActiveShiftCashflowHistory, getAllShifts, getShiftById } from "./queries";
+import { getActiveShift, getShiftCashflowHistory, getAllShifts, getShiftById } from "./queries";
 
 export type ActiveShift = NonNullable<Awaited<ReturnType<typeof getActiveShift>>>;
 export type ShiftHistory = NonNullable<Awaited<ReturnType<typeof getAllShifts>>>[number];
-export type CashflowHistory = NonNullable<Awaited<ReturnType<typeof getActiveShiftCashflowHistory>>>[number];
+export type CashflowHistory = NonNullable<Awaited<ReturnType<typeof getShiftCashflowHistory>>>[number];
 export type ShiftDetails = NonNullable<Awaited<ReturnType<typeof getShiftById>>>;
