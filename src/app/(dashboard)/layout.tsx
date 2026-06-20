cd3d4a63e363e3e1f21887ec4565c5a17e60de16
Gemah Ghozali <gemahghozali@gmail.com>
2026-06-20 09:53:22 +0800

    refactor(dashboard) : use new query function to get user and active shift id in dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,23 +1,18 @@
-import { redirect } from "next/navigation";
-import { getSession } from "@/libs/session";
-import { getActiveShiftId } from "@/features/shift/queries";
 import BottomNavigationBar from "@/component/bottom-navigation-bar";
 import ShiftGate from "@/features/shift/components/shift-gate";
+import { getCurrentUserAndShiftId } from "@/features/shift/queries";
 
 export default async function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const session = await getSession();
-  if (!session) redirect("/");
-
-  const isShiftActive = await getActiveShiftId();
+  const { user, shiftId } = await getCurrentUserAndShiftId();
 
   return (
     <div className="flex h-full flex-col">
       <div className="flex-1 overflow-y-auto relative scrollbar-hidden">
-        <ShiftGate isShiftActive={Boolean(isShiftActive)} userName={session.name}>
+        <ShiftGate isShiftActive={Boolean(shiftId)} userName={user.name}>
           {children}
         </ShiftGate>
       </div>
-      <BottomNavigationBar role={session.role} />
+      <BottomNavigationBar role={user.role} />
     </div>
   );
 }
